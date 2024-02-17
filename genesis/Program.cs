fc2d3abd95d9c8a0d117e01021b8cadf9ceb54de
Csoldo <nagyboldi2000@gmail.com>
2024-02-17 18:41:05 +0100

    initial commit

--- genesis/Program.cs
+++ genesis/Program.cs
@@ -25,13 +25,13 @@ namespace genesis
             
 
             string specimenZero = "lorem ipsum dolor sit amet consectetur adipiscing elit";
-            string fasz = "kitalalando string barmilyen hosszu lehet, de a fitness fuggvenyeket kell megirni hozza, es a mutatorokat, selectort, breederet, rendereret, es a godot, meg a simulatorokat, es a gene interface-t, es a gene implementaciokat";
+            string spec = "kitalalando string barmilyen hosszu lehet, de a fitness fuggvenyeket kell megirni hozza, es a mutatorokat, selectort, breederet, rendereret, es a godot, meg a simulatorokat, es a gene interface-t, es a gene implementaciokat";
 
             string test = "szia bazdmeg kutyaidat setaltatod";
             string asd = "random string ";
 
             God god = new God(2500, specimenZero, selector, mutator, fitnessCalculator, breeder, renderer);
-            God god2 = new God(1000, fasz, poolSelector, mutator, exponentialFitnessCalculator, breeder, renderer);
+            God god2 = new God(1000, spec, poolSelector, mutator, exponentialFitnessCalculator, breeder, renderer);
             God god3 = new God(2500, specimenZero, tournamentSelector, mutator, fitnessCalculator, breeder, renderer);
             //god3.run();
             Simulator simulator = new Simulator(god, 100);
@@ -40,7 +40,7 @@ namespace genesis
             Console.WriteLine("Number of specimens: " + god.getNumberOfSpecimens());
 
             DateTime timer2 = DateTime.Now;
-            //Console.WriteLine("Fasz: " + (timer2 - timer));
+            //Console.WriteLine("Time: " + (timer2 - timer));
 
             //TODO: Poolos selector, az a Breeder ahol szamit h milyen kozel van hozza + FitnessCalculator, Simulator, Tournament selector elitism
             //gene interface
